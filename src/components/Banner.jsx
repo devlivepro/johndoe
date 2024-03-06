6b509c1f954217779fffb0b2b62550a3409e5334
david <tyson.b@hotmail.fr>
2024-03-06 11:05:36 +0100

    Add news component banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,10 @@
+// BannerComponent
+import React from "react";
+
+function BannerComponent() {
+  return (
+    <div className="navbar navbar-expand-lg text-center color-official4 border-section-title banner-section-bg"></div>
+  );
+}
+
+export default BannerComponent;

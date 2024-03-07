11216e83946a434c040556251f926f60a8896cf7
david <tyson.b@hotmail.fr>
2024-03-07 12:22:41 +0100

    Updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
       <main className="container mt-4">
         <div className="col-md-12 mb-4">
           <div className="card text-center">
-          <h1 className="text-center bold">Github user</h1>
+            <h1 className="text-center bold">Github user</h1>
             <p>
               {loading ? (
                 <p>Loading...</p>
@@ -39,17 +39,29 @@ const About = () => {
                   <h2 className="semi-bold">{userData.name}</h2>
                   <img src={userData.avatar_url} alt="Avatar" width="150" />
                   <p className="color-official3">{userData.bio}</p>
-                  <p className="color-official3">Followers: {userData.followers}</p>
-                  <p className="color-official3">Following: {userData.following}</p>
-                  <p className="color-official3">Created on: {userData.created_at}</p>
-                  <p className="color-official3">Modified on: {userData.updated_at}</p>
-                  <a
-                    href={userData.html_url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    URL repositories : https://api.github.com/users/github-john-doe/repos
-                  </a>
+                  <p className="color-official3">
+                    Followers: {userData.followers}
+                  </p>
+                  <p className="color-official3">
+                    Following: {userData.following}
+                  </p>
+                  <p className="color-official3">
+                    Created on: {userData.created_at}
+                  </p>
+                  <p className="color-official3">
+                    Modified on: {userData.updated_at}
+                  </p>
+                  <p className="color-official3">
+                    URL repositories:{" "}
+                    <a
+                      className="color-official1"
+                      href={userData.html_url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      https://api.github.com/users/github-john-doe/repos
+                    </a>
+                  </p>
                 </div>
               )}
             </p>
